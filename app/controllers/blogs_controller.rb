6e11461ece1d6b3c817c2ce3b20f81a0626c1233
nokia0315 <naoki11923611@gmail.com>
2020-11-11 07:31:28 +0000

    [finish]3-4

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,7 @@
 class BlogsController < ApplicationController
+
   def index
+    @blogs = Blog.all
   end
 
   def show
@@ -17,10 +19,10 @@ class BlogsController < ApplicationController
 
   def edit
   end
-  
+
   private
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
   end
-  
+
 end
